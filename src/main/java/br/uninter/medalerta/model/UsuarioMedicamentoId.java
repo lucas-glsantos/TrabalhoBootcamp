ede625a7b9942bf17edb0cf5eb02673b04d824d7
laionCosta <laionsilverio@gmail.com>
2026-04-16 14:49:16 -0300

    refactor: substitui boilerplate por anotações Lombok nas entities
    - Usuario: @Getter, @Setter, @NoArgsConstructor, @ToString
    - Medicamento: @Getter, @Setter, @NoArgsConstructor, @ToString
    - UsuarioMedicamento: @Getter, @Setter, @NoArgsConstructor (mantém toString customizado)
    - UsuarioMedicamentoId: @Getter, @Setter, @NoArgsConstructor, @AllArgsConstructor, @EqualsAndHashCode
    
    Reduz ~210 linhas de código repetitivo(getters/setters/construtores).

--- src/main/java/br/uninter/medalerta/model/UsuarioMedicamentoId.java
+++ src/main/java/br/uninter/medalerta/model/UsuarioMedicamentoId.java
@@ -2,11 +2,20 @@ package br.uninter.medalerta.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 @Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class UsuarioMedicamentoId implements Serializable {
 
     @Column(name = "idUsuario")
@@ -14,41 +23,4 @@ public class UsuarioMedicamentoId implements Serializable {
 
     @Column(name = "idMedicamento")
     private Integer idMedicamento;
-
-    public UsuarioMedicamentoId() {
-    }
-
-    public UsuarioMedicamentoId(Integer idUsuario, Integer idMedicamento) {
-        this.idUsuario = idUsuario;
-        this.idMedicamento = idMedicamento;
-    }
-
-    public Integer getIdUsuario() {
-        return idUsuario;
-    }
-
-    public void setIdUsuario(Integer idUsuario) {
-        this.idUsuario = idUsuario;
-    }
-
-    public Integer getIdMedicamento() {
-        return idMedicamento;
-    }
-
-    public void setIdMedicamento(Integer idMedicamento) {
-        this.idMedicamento = idMedicamento;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof UsuarioMedicamentoId that)) return false;
-        return Objects.equals(idUsuario, that.idUsuario) &&
-                Objects.equals(idMedicamento, that.idMedicamento);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(idUsuario, idMedicamento);
-    }
 }
